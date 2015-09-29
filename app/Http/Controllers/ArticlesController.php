a939b82e4fd7b53b8f49fa26c5bdd42b6449449f
kvdimitrov <kalin.vl.dimitrov@abv.bg>
2015-09-29 13:32:03 +0300

    Forms

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -2,22 +2,37 @@
 
 namespace App\Http\Controllers;
 use App\Article;
-use Illuminate\Http\Request;
-use App\Http\Requests;
+//use Illuminate\Http\Request;
+// use App\Http\Requests;
+use Request;
+use Carbon\Carbon;
 use App\Http\Controllers\Controller;
 
 class ArticlesController extends Controller
 {
     public function index(){
-        $articles = Article::all();
+        $articles = Article::latest('published_at')->get();
         
         return view('pages/articles', compact('articles'));
     }
     
     public function show($id){
-        $article = Article::findOrFail($id);
-        
+        $article = Article::findOrFail($id);       
         
         return view('pages/show', compact('article'));
+    }
+    
+    public function create(){
+        return view('pages/create');
+    }
+    
+    public function store(){
+        
+        $input = Request::all();
+        $input['published_at'] = Carbon::now();
+        
+        Article::create($input);
+        
+        return redirect('articles');
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,4 +14,6 @@
 Route::get('/', 'IndexController@index');
 Route::get('about', 'PagesController@index');
 Route::get('articles', 'ArticlesController@index');
+Route::get('articles/create', 'ArticlesController@create');
 Route::get('articles/{id}', 'ArticlesController@show');
+Route::post('articles', 'ArticlesController@store');
