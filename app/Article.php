2582804814998bf33a9ea39aa9f1532ce1105097
kvdimitrov <kalin.vl.dimitrov@abv.bg>
2015-09-29 09:53:38 +0300

    Article

--- app/Article.php
+++ app/Article.php
@@ -4,11 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Article extends Model
-{
+class Article extends Model {
+
     protected $fillable = [
         'title',
         'body',
         'published_at'
     ];
+
 }
