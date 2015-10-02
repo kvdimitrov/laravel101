e7c5657f7bd1362ce81b91997896d39ac4d5f1f3
kvdimitrov <kalin.vl.dimitrov@abv.bg>
2015-10-02 11:41:24 +0300

    Manage Your Assets

--- resources/views/templates/app.blade.php
+++ resources/views/templates/app.blade.php
@@ -3,7 +3,8 @@
     <head>
         <title>Laravel</title>
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <link rel="stylesheet" href="{{ elixir('css/all.css') }}">
         @yield('head')
     </head>
     <body>
